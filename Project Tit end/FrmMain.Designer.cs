8727363d8cc2d753eaf03d718441710836279976
ehsanshirzadi <ehsan.shirzadi74@gmail.com>
2020-03-11 22:24:05 +0330

    Start

---
+++ Project Tit end/FrmMain.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Project_Tit_end
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.txtBrowser = new System.Windows.Forms.TextBox();
+            this.fBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.pbStart = new System.Windows.Forms.ProgressBar();
+            this.lbFiles = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtSearch.Location = new System.Drawing.Point(4, 44);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(349, 30);
+            this.txtSearch.TabIndex = 2;
+            // 
+            // txtBrowser
+            // 
+            this.txtBrowser.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtBrowser.Location = new System.Drawing.Point(4, 5);
+            this.txtBrowser.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtBrowser.Name = "txtBrowser";
+            this.txtBrowser.ReadOnly = true;
+            this.txtBrowser.Size = new System.Drawing.Size(349, 30);
+            this.txtBrowser.TabIndex = 3;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 62.90801F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37.09199F));
+            this.tableLayoutPanel1.Controls.Add(this.btnBrowser, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnSearch, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtBrowser, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtSearch, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(569, 79);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnBrowser.Location = new System.Drawing.Point(361, 5);
+            this.btnBrowser.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(204, 29);
+            this.btnBrowser.TabIndex = 0;
+            this.btnBrowser.Text = "&Browser";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSearch.Location = new System.Drawing.Point(361, 44);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(204, 30);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "&Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.pbStart, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.lbFiles, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 85);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 92.28571F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.714286F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(569, 350);
+            this.tableLayoutPanel2.TabIndex = 7;
+            // 
+            // pbStart
+            // 
+            this.pbStart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbStart.Location = new System.Drawing.Point(3, 325);
+            this.pbStart.Name = "pbStart";
+            this.pbStart.Size = new System.Drawing.Size(563, 22);
+            this.pbStart.Step = 1;
+            this.pbStart.TabIndex = 10;
+            // 
+            // lbFiles
+            // 
+            this.lbFiles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbFiles.FormattingEnabled = true;
+            this.lbFiles.ItemHeight = 23;
+            this.lbFiles.Location = new System.Drawing.Point(3, 3);
+            this.lbFiles.Name = "lbFiles";
+            this.lbFiles.Size = new System.Drawing.Size(563, 316);
+            this.lbFiles.TabIndex = 9;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 435);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MinimizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TiT";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.TextBox txtBrowser;
+        private System.Windows.Forms.FolderBrowserDialog fBrowser;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.ProgressBar pbStart;
+        private System.Windows.Forms.ListBox lbFiles;
+    }
+}
+
---
+++ Project Tit end/FrmMain.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Project_Tit_end
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var resul = fBrowser.ShowDialog();
+                if (resul == DialogResult.OK) txtBrowser.Text = fBrowser.SelectedPath;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex + "خطای رخ داده است ");
+            }
+        }
+        private string MakeReg(string e)
+        {
+            try
+            {
+                string reg = "";
+                var sp = e.ToLower().Split(' ').ToList();
+                foreach (var item in sp)
+                {
+                    if (item == "or")
+                    {
+                        reg = reg + "|";
+                    }
+                    else if (item == "and")
+                    {
+                        reg = reg + "\\s*";
+                    }
+                    else if (item == "") continue;
+                    else
+                    {
+                        reg = reg + "(" + item + ")";
+                    }
+                }
+                return reg;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex + "خطای رخ داده است ");
+                return "";
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lbFiles.Items.Clear();
+                if (string.IsNullOrEmpty(txtBrowser.Text)) return;
+                var fi = Directory.GetFiles(txtBrowser.Text, "*.txt").ToList();
+                if (fi.Count == 0)
+                {
+                    MessageBox.Show("آدرس مورد نظر حاوی فایل متنی نیست");
+                    return;
+                }
+                pbStart.Value = 0;
+                pbStart.Maximum = fi.Count;
+                if (string.IsNullOrWhiteSpace(txtSearch.Text)) MessageBox.Show("عبارت مورد جستجو را وارد کنید");
+                foreach (var item in fi)
+                {
+                    var fii = File.ReadAllText(item).ToLower();
+                    var myreg = MakeReg(txtSearch.Text);
+                    var mymatch = DoMatch(fii, myreg);
+                    lbFiles.Items.Add(item + "  " + mymatch);
+                    pbStart.Value += 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex + "خطای رخ داده است ");
+            }
+        }
+        private bool DoMatch(string mainText, string reg)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(reg)) return false;
+                if (Regex.IsMatch(mainText, reg))
+                {
+                    return true;
+
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex + "خطای رخ داده است ");
+                return false;
+            }
+        }
+    }
+}
